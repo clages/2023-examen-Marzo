66933e2732ef8955ceda8391662093ed8669540b
Mario Rampi <13686838+MarioAr@users.noreply.github.com>
2023-03-03 08:41:58 -0300

    Ejercicio 1

---
+++ uno.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Se debe realizar la carga de 5 ventas de productos de almacen, los datos a cargar de cada producto son:
+ * marca (validar que se ingrese un dato), el precio (entre 300 y 2000), las unidades (entre 1 y 10) 
+ * y el tipo (yerba, azucar, galletitas)
+ * Informar lo siguiente:
+ * a) El monto total vendido de yerba.
+ * b) La marca y cantidad de unidades de el azucar mas barato.
+ * c) Del tipo con mas unidades vendidas el promedio de cada compra.
+ * 
+ */
+function mostrar()
+{
+	
+}
